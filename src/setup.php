e4d1816ac1647878775e977c9aa83a0628239fc8
Jordan Hall <jordan.hall@rapidweb.biz>
2018-02-20 14:46:21 +0000

    Fix to vendor autoload require

--- src/setup.php
+++ src/setup.php
@@ -2,7 +2,7 @@
 
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../vendor/autoload.php';
-} elseif (file_exists(__DIR__.'/../../../../autoload.php')) {
+} elseif (file_exists(__DIR__.'/../../../../vendor/autoload.php')) {
     require_once __DIR__.'/../../../../vendor/autoload.php';
 }
 
