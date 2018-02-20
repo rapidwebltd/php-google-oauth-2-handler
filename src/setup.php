f9a1360c40c203fc60def161810e8d0aa37932b7
Jordan Hall <jordan.hall@rapidweb.biz>
2018-02-20 14:35:41 +0000

    Add copy snippet output, and fix reference to  GoogleOAuth2Handler

--- src/setup.php
+++ src/setup.php
@@ -6,6 +6,8 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../../../../vendor/autoload.php';
 }
 
+use RapidWeb\GoogleOAuth2Handler\GoogleOAuth2Handler;
+
 echo PHP_EOL;
 echo '*** PHP Google OAuth 2 Handler - Setup ***';
 echo PHP_EOL.PHP_EOL;
@@ -49,3 +51,18 @@ echo PHP_EOL;
 echo 'access to the Google account.';
 echo PHP_EOL.PHP_EOL;
 
+echo 'You can now create a GoogleOAuth2Handler object for this Google';
+echo PHP_EOL;
+echo 'account using the following code.';
+echo PHP_EOL.PHP_EOL;
+
+echo '$clientId     = \''.$clientId.'\';';
+echo PHP_EOL;
+echo '$clientSecret = \''.$clientSecret.'\';';
+echo PHP_EOL;
+echo '$refreshToken = \''.$refreshToken.'\';';
+echo PHP_EOL;
+echo '$scopes       = []; // Set required scopes. Available scopes: https://developers.google.com/identity/protocols/googlescopes';
+echo PHP_EOL.PHP_EOL;
+echo '$googleOAuth2Handler = new GoogleOAuth2Handler($clientId, $clientSecret, $scopes, $refreshToken);';
+echo PHP_EOL.PHP_EOL;
